3bbb18f2495df37da0a1f732af055f41c1599c86
Tanaphat <tanaphatkaewsri@gmail.com>
2023-10-16 17:20:23 +0700

    update 161066

--- angular-deep/src/app/app.component.ts
+++ angular-deep/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-deep';
+  inputText: string = '';
+
+  OnSubmit(inputEl: HTMLInputElement) {
+    this.inputText = inputEl.value;
+  }
 }
---
+++ angular-deep/src/app/demo/demo.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css'],
+})
+export class DemoComponent {
+  @Input() value: string = 'procademy';
+}
